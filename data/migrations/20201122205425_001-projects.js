6ecc3fe0e568acaa987db9f5f042d9a257ddf757
Matthew Martin <zhintateo19924559@gmail.com>
2020-11-22 21:10:26 -0600

    Created the Knex migration file and pulled in dotenv, sqlite3

---
+++ data/migrations/20201122205425_001-projects.js
@@ -0,0 +1,46 @@
+
+exports.up = function(knex) {
+  return knex.schema
+      .createTable('projects', function (projects) {
+          projects.increments();
+          projects.string("project_name", 128).notNullable().unique()
+          projects.string("project_desc", 255)
+          projects.boolean("completed").notNullable().defaultTo(false)
+      })
+
+        .createTable("resources", (resources) => {
+            resources.increments()
+            resources.string("resource_name", 128).notNullable().unique()
+            resources.string("resource_desc", 255)
+        })
+
+      .createTable("tasks", (tasks) => {
+          tasks.increments()
+          tasks.string("tasks_desc").notNullable().unique()
+          tasks.string("tasks_notes", 128)
+      })
+
+      .createTable("project_resource", (pR) => {
+          pR.integer("project_id")
+              .references("id")
+              .inTable("projects")
+              .onDelete("CASCADE")
+              .onUpdate("CASCADE")
+              .notNullable()
+          pR.integer("resource_id")
+              .references("id")
+              .inTable("resources")
+              .onDelete("CASCADE")
+              .onUpdate("CASCADE")
+              .notNullable()
+          pR.primary(['project_id', 'resource_id'])
+      })
+
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("projects")
+      .dropTableIfExists("tasks")
+      .dropTableIfExists("resources")
+      .dropTableIfExists("project_resource")
+};
